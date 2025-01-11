cecc536407f3076964afcc242eb67aa0799c48f8
joss-thu <antojo01@thu.de>
2025-01-11 15:19:49 +0100

    websockets example with socket.id assigned.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const Websocket = require('ws')
 const server = new Websocket.Server({ port: 8080 });
+const { v4: uuidv4 } = require('uuid');
 
 server.on('connection', (socket) => {
+    socket.id = uuidv4();
     console.log(`Client connected: ${socket.id}`);
     socket.on('message', (message) => {
-        console.log(`Server log: ${message}`);
+        console.log(`Message received from ${socket.id}: ${message}`);
         socket.send(`Server says: ${message}`);
+    })
+    socket.on('close', () => {
+        console.log(`Client disconnected: ${socket.id}`);
     })
 })
