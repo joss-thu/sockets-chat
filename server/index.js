dcc40c5c7e9e2cf2ea1264ed19cfa411f5881c76
joss-thu <antojo01@thu.de>
2025-01-11 15:00:37 +0100

    Basic example for webrtc done.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const Websocket = require('ws')
 const server = new Websocket.Server({ port: 8080 });
 
 server.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
     socket.on('message', (message) => {
         console.log(`Server log: ${message}`);
         socket.send(`Server says: ${message}`);
